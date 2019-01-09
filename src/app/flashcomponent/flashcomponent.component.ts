51aae81a99de9ec08afb73c79a880866a528c4da
liciolentimo <lentimo@gmail.com>
2019-01-09 17:42:47 +0300

    Added title and toggle button

--- src/app/flashcomponent/flashcomponent.component.ts
+++ src/app/flashcomponent/flashcomponent.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Flash } from '../flash';
+
 
 @Component({
   selector: 'app-flashcomponent',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flashcomponent.component.css']
 })
 export class FlashcomponentComponent implements OnInit {
+  flashcards = [
+    new Flash('Object','An instance of a class'),
+    new Flash('Abstraction',' Its main goal is to handle complexity by hiding unnecessary details from the user.')
+  ]
+
+  toggleDetails(index){
+    this.flashcards[index].showDescription = !this.flashcards[index].showDescription;
+}
 
   constructor() { }
 
--- src/app/flashdetails/flashdetails.component.ts
+++ src/app/flashdetails/flashdetails.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Flash } from '../flash';
 
 @Component({
   selector: 'app-flashdetails',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlashdetailsComponent implements OnInit {
 
+  @Input() flashcard:Flash;
   constructor() { }
 
   ngOnInit() {
